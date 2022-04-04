caa93d8af6a039f6f78eafabd62d874abdad7f60
Sumejja Fazlic <sumejjafazlic@hotmail.com>
2022-04-04 16:02:41 +0200

    Add Exercise 02

---
+++ ex02/src/index.js
@@ -0,0 +1,6 @@
+function functionPrintInfo(parameter1, parameter2){
+    console.log(parameter1, parameter2);
+    }
+    
+    module.exports = functionPrintInfo;
+    functionPrintInfo("John", 23);
